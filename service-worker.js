22a9d96cf9f3f377b3656905343d0c716ba4e732
Oleksii Spalylo <white.rh42@gmail.com>
2021-01-20 10:43:44 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.1fcb629dae1c8df5f6f61ae7751aeb90.js"
 );
 
 self.addEventListener('message', (event) => {
